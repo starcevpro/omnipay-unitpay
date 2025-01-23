716698a567c560305342709e35fe2c9717f12c5b
starcevpro <artcraftden@gmail.com>
2025-01-23 13:53:34 +0300

    Uplod v1

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -4,13 +4,57 @@ namespace Omnipay\UnitPay\Message;
 
 class CompletePurchaseRequest extends AbstractUnitPayRequest
 {
+    protected $allowedIps;
+
+    public function setAllowedIps($value)
+    {
+        $this->allowedIps = $value;
+        return $this;
+    }
+
     public function getData(): array
     {
-        return $this->httpRequest->request->all();
+        $data = $this->httpRequest->request->all();
+
+        if (empty($data)) {
+            $data = $this->httpRequest->query->all();
+        }
+
+        if (!isset($data['method']) || !isset($data['params'])) {
+            throw new \InvalidArgumentException('Invalid request format');
+        }
+
+        if ($this->allowedIps) {
+            $data['allowedIps'] = $this->allowedIps;
+        }
+
+        if (is_array($data['params'])) {
+            return [
+                'method' => $data['method'],
+                'params' => $data['params']
+            ];
+        }
+
+        $params = [];
+        foreach ($data as $key => $value) {
+            if (str_starts_with($key, 'params.')) {
+                $paramKey = substr($key, 7);
+                $params[$paramKey] = $value;
+            }
+        }
+
+        return [
+            'method' => $data['method'],
+            'params' => $params
+        ];
     }
 
     public function sendData($data): UnitPayResponseInterface
     {
-        return new CompletePurchaseResponse($this, $data);
+        $response = new CompletePurchaseResponse($this, $data);
+        if ($this->allowedIps) {
+            $response->setAllowedIps($this->allowedIps);
+        }
+        return $response;
     }
 }
--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -6,26 +6,93 @@ use Omnipay\Common\Message\AbstractResponse;
 
 class CompletePurchaseResponse extends AbstractResponse implements UnitPayResponseInterface
 {
+    protected array $allowedIps = [
+        '31.186.100.49',
+        '178.132.203.105',
+        '52.29.152.23',
+        '52.19.56.234',
+    ];
+
     public function isSuccessful(): bool
     {
+        if (!$this->isAllowedIp()) {
+            return $this->error('Неверный IP-адрес');
+        }
+
         if (!isset($this->data['method']) || !isset($this->data['params'])) {
-            return false;
+            return $this->error('Неверный формат запроса');
         }
 
-        if ($this->data['method'] !== 'pay') {
-            return false;
-        }
-
+        $method = $this->data['method'];
         $params = $this->data['params'];
-        $signature = $params['signature'] ?? null;
-        unset($params['signature']);
 
-        ksort($params);
-        $params[] = $this->request->getSecretKey();
+        if (!$this->verifySignature($method, $params)) {
+            return $this->error('Неверная подпись');
+        }
 
-        $localSignature = hash('sha256', implode('{up}', $params));
+        switch ($method) {
+            case 'check':
+                return $this->success('Запрос успешно обработан');
+            case 'pay':
+                return $this->success('Платеж успешно обработан');
+            case 'preauth':
+                if (!isset($params['isPreauth']) || $params['isPreauth'] != 1) {
+                    return $this->error('Неверный запрос преавторизации');
+                }
+                return $this->success('Преавторизация успешно обработана');
+            case 'error':
+                $errorMessage = $params['errorMessage'] ?? 'Неизвестная ошибка';
+                return $this->success('Ошибка обработана: ' . $errorMessage);
+            default:
+                return $this->error('Неверный метод');
+        }
+    }
 
-        return hash_equals($localSignature, $signature);
+    public function setAllowedIps(array $ips): void
+    {
+        $this->allowedIps = $ips;
+    }
+
+    protected function isAllowedIp(): bool
+    {
+        $ip = $_SERVER['REMOTE_ADDR'] ?? '';
+        return in_array($ip, $this->allowedIps);
+    }
+
+    protected function verifySignature(string $method, array $params): bool
+    {
+        if (empty($params['signature'])) {
+            return false;
+        }
+
+        $signatureParams = $params;
+        unset($signatureParams['signature']);
+        ksort($signatureParams);
+
+        $signString = $method . '{up}' . implode('{up}', $signatureParams) . '{up}' . $this->request->getSecretKey();
+        return hash_equals(
+            hash('sha256', $signString),
+            $params['signature']
+        );
+    }
+
+    protected function success(string $message): bool
+    {
+        $this->data['response'] = [
+            'result' => ['message' => $message]
+        ];
+        return true;
+    }
+
+    protected function error(string $message): bool
+    {
+        $this->data['response'] = [
+            'error' => [
+                'message' => $message,
+                'locale' => $this->data['params']['locale'] ?? 'ru'
+            ]
+        ];
+        return false;
     }
 
     public function getTransactionReference()
@@ -46,5 +113,16 @@ class CompletePurchaseResponse extends AbstractResponse implements UnitPayRespon
     public function getCurrency()
     {
         return $this->data['params']['orderCurrency'] ?? null;
+    }
+
+    public function getMessage(): ?string
+    {
+        if (isset($this->data['response']['result'])) {
+            return $this->data['response']['result']['message'];
+        }
+        if (isset($this->data['response']['error'])) {
+            return $this->data['response']['error']['message'];
+        }
+        return null;
     }
 }
